52193c8a0950c320b101a981948e10b2f83d666c
Priyadharshini <priyadharshinijoo2006@gmail.com>
2026-07-09 12:22:06 +0530

    Initial Spring Boot Project Setup

---
+++ DeveloperGrowthPlatform/src/main/java/com/developer/platform/DeveloperGrowthPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.developer.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperGrowthPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperGrowthPlatformApplication.class, args);
+	}
+
+}
